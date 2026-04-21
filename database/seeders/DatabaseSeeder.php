58ed0b510fb507c1da9fa234fb45e80e5e535447
Riyanto4 <riyantoo2425@gmail.com>
2026-04-21 16:08:56 +0700

    update seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,10 +21,10 @@ class DatabaseSeeder extends Seeder
             'email' => 'test@example.com',
         ]);
 
-        $this->call([
+      /*   $this->call([
             UserSeeder::class,
             ProductSeeder::class,
             TransactionSeeder::class,
-        ]);
+        ]); */
     }
 }
